80e484f1144eded70b84863ff835a0651ae7c783
JakubKwiatek132 <126687451+JakubKwiatek132@users.noreply.github.com>
2023-05-24 12:44:16 +0000

    długość łańcucha

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    Scanner o = new Scanner(System.in);
+        System.out.println("Podaj ciag znaków");
+        String a = o.nextLine();
+
+        int len = a.length();
+    System.out.println(len);
   }
 }
